3bcf1ba7b547a1bf04e9ca1927dd81f572403f88
mrlogic <mrlogic1987@gmail.com>
2025-08-31 04:37:55 +0100

    commit to gitup

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from views import views
-import dotenv
+from dotenv import load_dotenv
+from flask_mail import Mail, Message
+import os
 app = Flask(__name__)
 
+load_dotenv()
 app.register_blueprint(views, url_prefix="/" )
+
+print(os.getenv('SCREAT_KEY'))
+
+app.config['MAIL_SERVER']= os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
+app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL')
+mail = Mail(app)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
